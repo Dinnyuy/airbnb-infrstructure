81c5281120f24690b3bf063a49e4db86ed4fdddc
Dinnyuy123 <dinnyuylemnyuy@gmail.com>
2023-05-24 05:10:55 +0100

    working on IAM role

--- EC2.tf
+++ EC2.tf
@@ -1,28 +1,14 @@
 resource "aws_instance" "web" {
   ami           = var.AMI_ID
   instance_type = var.INSTANCE_TYPE
+  iam_instance_profile = "test"
   monitoring = true 
   ebs_optimized = true
   root_block_device {
     encrypted     = true
  }
-
- resource "aws_instance" "pass" {
-  ami           = "ami-0149b2da6ceec4bb0" # 
-  instance_type = "t2.micro"
-  iam_instance_profile = "test"
-
-  network_interface {
-    network_interface_id = aws_network_interface.foo.id
-    device_index         = 0
-  }
-
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
-}
-
   tags = {
+    
     Name = "air-bnb-web-server-instance"
   }
 }
